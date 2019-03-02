f8bbe531ca3ecf709a6e6fbbd52b4fb0fc61123e
Travis Williamson <cthwgw1791@gmail.com>
2019-03-02 01:35:45 -0800

    getting logic built slowly but surely

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# gify-hw-week-6
-A Little Gify Work
+# giphy-hw-week-6
+
+## A Little Giphy Work
+
+### A Brief Synopsis:
+
+A fun dive into the world of GIFs. User can click the pre-selected GIF buttons or search for her own and dynamically add that button to the list! But wait, there's more! The user will be able to start or stop the GIFs on click. How exciting is that? 
+
+For more info on this API and how to use it, you can [visit the GIPHY site!](https://developers.giphy.com/docs/).
---
+++ assets/logic.js
@@ -0,0 +1,47 @@
+/**********************************************
+ * What? A GIF site. Does? Neat stuff. 
+ * The user will be able to click on pre-selected themes to pull some GIFs from our API. 
+ * If she clicks the same button again, more GIFs will prepend to the screen.
+ * User should be able to search for her own GIFs.
+ * User should be able to click on GIFs to animate and stop
+ * 
+ * To-do:
+ * create link to API
+ * create function to pull the info
+ * Create an array of GIF themes for buttons
+ * on click display 10 pics
+ * on second click prepend 10 more
+ * click on GIF to stop or animate
+ * create input box for user to add search button for her own GIF theme
+ * create a clear button to reset back to default
+ */
+
+//Array of Pre-selected GIF themes
+var preSelectedGif = ['Pets', 'Ribald',];
+
+// DisplayGifInfo function will pull the gifs from the giphy.com and then pushes it to the html
+function displayGifInf() {
+
+    // Create variable for storing the pre-selected themes from the API
+    var gifTheme = $(this).attr("data-gif");
+    // Create variable to  grab the data from the API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTheme + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // Creating AJAX call for the GIF theme button being clicked
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?q=cats&api_key=dc6zaTOxFJmzC&limit=10",
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+
+    // // Creating variable to store data from the AJAX request
+    // var ajaxGifResults = response.data;
+
+    // // Creating a loop to cycle through each result item
+    // forEach(function(ajaxGifRuslts) {
+        
+
+    // })
+
+}
