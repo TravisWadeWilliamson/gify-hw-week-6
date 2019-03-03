0aa68e64f9768a7301fe0af9e8205bdfadf2fbd3
Travis Williamson <cthwgw1791@gmail.com>
2019-03-02 23:45:23 -0800

    Changed everything cos I'm just having a tough time with something that seems so fucking simple. ARRRRG

--- assets/logic.js
+++ assets/logic.js
@@ -17,96 +17,123 @@
  */
 
 
-//Array of Pre-selected GIF themes
-var preSelectedGif = ['Pets', 'Ribald',];
-var gifSearch = 'cats'
+//Adding click even listener
+$('button').on('click', function () {
 
-//DisplayGifInfo function will pull the gifs from the giphy.com and then pushes it to the html
+    //Grabbing and storing the data-animal property from the button
+    var animal = $(this).attr('data-animal');
 
+    // Create variable to  grab the data from the API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-// Create variable for storing the pre-selected themes from the API
-var gifTheme = $(this).attr("data-gif");
-// Create variable to  grab the data from the API
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
+    // Creating AJAX call for the GIF theme button being clicked and log the response
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-// Creating AJAX call for the GIF theme button being clicked
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
+        // Storing the data from the AJAX request in a results variable
+        var results = response.data
+        console.log(results);
 
-// Creating variable to store data from the AJAX request
-var ajaxGifResults = response.data;
+        // // Loops through the AJAX response
+        results.forEach(result => {
 
-// Creating a loop to cycle through each result item
-for (var i = 0; i < ajaxGifResults.length; i++) {
+            //Create and store a div tag
+            var animalDiv = $('<div>');
 
-    // Creating a div to hold the GIF 
-    var gifDiv = $("<div class='gif'>")
+            //Create the paragraph tag with the rating of the GIF.
+            var rating = $(`<p>`).text(`Rated: ${result.rating}`);
+            
+            //Create and store an image tag
+            var animalImage = $(`<img>`);
 
-    // Creating a p tag with the result item's rating
-    var ratingP = $("<p>").text(`Rated: ${ajaxGifResults[i].rating}`);
+            //Setting the src attribute of the image to a property pulled off the result item
 
-    // Displaying the rating
-    gifDiv.append(ratingP);
+            animalImage.attr("src", result.images.fixed_height.url);
 
-    // Retrieving the URL for the image
-    var gifURL = ajaxGifResults[i].images.fixed_height.url;
+            //Append the p tag and image tag to the animalDiv element
+            animalDiv.append(rating);
+            animalDiv.append(animalImage);
 
-    // Creating an element to hold the image
-    var gifImage = $("<img>").attr("src", gifURL);
+            //Prepending the animalDiv to the html page in the #gifs-go-here div
+            $(`#gifs-go-here`).prepend(animalDiv);
+            
+        });
+    });
+})
+        
 
-    // Appending the paragraph and image tag to the animalDiv
-    gifDiv.append(ratingP);
-    gifDiv.append(gifImage);
+// // Creating variable to store data from the AJAX request
+// var ajaxGifResults = response.data;
 
-    // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-    $("#gifs-go-here").prepend(gifDiv);
+// // Creating a loop to cycle through each result item
+// for (var i = 0; i < ajaxGifResults.length; i++) {
 
-};
+//     // Creating a div to hold the GIF 
+//     var gifDiv = $("<div class='gif'>")
 
-// Function for displaying movie data
-function renderButtons() {
+//     // Creating a p tag with the result item's rating
+//     var ratingP = $("<p>").text(`Rated: ${ajaxGifResults[i].rating}`);
 
-    // Deleting the movie buttons prior to adding new movie buttons
-    // (this is necessary otherwise we will have repeat buttons)
-    $("#buttons-view").empty();
+//     // Displaying the rating
+//     gifDiv.append(ratingP);
 
-    // Looping through the array of movies
-    for (var i = 0; i < preSelectedGif.length; i++) {
+//     // Retrieving the URL for the image
+//     var gifURL = ajaxGifResults[i].images.fixed_height.url;
 
-        // Then dynamicaly generating buttons for each movie in the array.
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class
-        a.addClass("gifButton");
-        // Adding a data-attribute with a value of the movie at index i
-        a.attr("data-name", preSelectedGif[i]);
-        // Providing the button's text with a value of the movie at index i
-        a.text(preSelectedGif[i]);
-        // Adding the button to the HTML
-        $("#buttons-view").append(a);
-    }
-}
-// This function handles events where one button is clicked
-$("#add-button").on("click", function (event) {
-    // event.preventDefault() prevents the form from trying to submit itself.
-    // We're using a form so that the user can hit enter instead of clicking the button if they want
-    event.preventDefault();
+//     // Creating an element to hold the image
+//     var gifImage = $("<img>").attr("src", gifURL);
 
-    // This line will grab the text from the input box
-    var preSelectedGif = $("#movie-input").val().trim();
-    // The movie from the textbox is then added to our array
-    movies.push(movie);
+//     // Appending the rating and image tag to the animalDiv
+//     gifDiv.append(ratingP);
+//     gifDiv.append(gifImage);
 
-    // calling renderButtons which handles the processing of our movie array
-    renderButtons();
-});
+//     // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+//     $("#gifs-go-here").prepend(gifDiv);
 
-// Calling the renderButtons function at least once to display the initial list of movies
-renderButtons();
+// };
+
+// // Function for displaying movie data
+// function renderButtons() {
+
+//     // Deleting the movie buttons prior to adding new movie buttons
+//     // (this is necessary otherwise we will have repeat buttons)
+//     $("#buttons-view").empty();
+
+//     // Looping through the array of movies
+//     for (var i = 0; i < preSelectedGif.length; i++) {
+
+//         // Then dynamicaly generating buttons for each movie in the array.
+//         // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+//         var a = $("<button>");
+//         // Adding a class
+//         a.addClass("gifButton");
+//         // Adding a data-attribute with a value of the movie at index i
+//         a.attr("data-name", preSelectedGif[i]);
+//         // Providing the button's text with a value of the movie at index i
+//         a.text(preSelectedGif[i]);
+//         // Adding the button to the HTML
+//         $("#buttons-view").append(a);
+//     }
+// }
+// // This function handles events where one button is clicked
+// $("#add-button").on("click", function (event) {
+//     // event.preventDefault() prevents the form from trying to submit itself.
+//     // We're using a form so that the user can hit enter instead of clicking the button if they want
+//     event.preventDefault();
+
+//     // This line will grab the text from the input box
+//     var preSelectedGif = $("#movie-input").val().trim();
+//     // The movie from the textbox is then added to our array
+//     movies.push(movie);
+
+//     // calling renderButtons which handles the processing of our movie array
+//     renderButtons();
+// });
+
+// // Calling the renderButtons function at least once to display the initial list of movies
+// renderButtons();
 
 
 
